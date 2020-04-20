ae75e69ee41743bf650485174c560583c28a4f7c
Jakub Strawa <j.strawa@stud.elka.pw.edu.pl>
2020-04-20 20:54:20 +0200

    Added Rotors to Enigma class

--- src/main/java/com/mycompany/mavenproject1/Enigma.java
+++ src/main/java/com/mycompany/mavenproject1/Enigma.java
@@ -11,11 +11,16 @@ package com.mycompany.mavenproject1;
  */
 public class Enigma {
     private Rotor rotor1,rotor2,rotor3,rotor4,rotor5;
-    private char [] tab1 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    private char [] tab2 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    private char [] tab3 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    private char [] tab4 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    private char [] tab5 = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+    //E K M F L G D Q V Z N T O W Y H X U S P A I B R C J
+    private char [] tab1 = {'e','k','m','f','l','g','d','q','v','z','n','t','o','w','y','h','x','u','s','p','a','i','b','r','c','j'};
+    //A J D K S I R U X B L H W T M C Q G Z N P Y F V O E
+    private char [] tab2 = {'a','j','d','k','s','i','r','u','x','b','l','h','w','t','m','c','q','g','z','n','p','y','f','v','o','e'};
+    //B D F H J L C P R T X V Z N Y E I W G A K M U S Q O
+    private char [] tab3 = {'b','d','f','h','j','l','c','p','r','t','x','v','z','n','y','e','i','w','g','a','k','m','u','s','q','o'};
+    //E S O V P Z J A Y Q U I R H X L N F T G K D C M W B
+    private char [] tab4 = {'e','s','o','v','p','z','j','a','y','q','u','i','r','h','x','l','n','f','t','g','k','d','c','m','w','b'};
+    //V Z B R G I T Y U P S D N H L X A W M J Q O F E C K
+    private char [] tab5 = {'v','z','b','r','g','i','t','y','u','p','s','d','n','h','l','x','a','w','m','j','q','o','f','e','c','k'};
 
     
     public Enigma(){
