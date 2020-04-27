e8aea7c8910b8307d03de52338c896da7dfa21bf
Jakub Strawa <j.strawa@stud.elka.pw.edu.pl>
2020-04-27 18:01:50 +0200

    AES artibutes added

--- src/main/java/com/mycompany/mavenproject1/AES.java
+++ src/main/java/com/mycompany/mavenproject1/AES.java
@@ -11,11 +11,26 @@ package com.mycompany.mavenproject1;
  */
 public class AES {
     private String message;
-    boolean isEncoded;
+    private boolean isEncoded;
+    private char [] block, key;
+        
     
-    public AES(String msg, boolean b){
+    public AES(String msg, String k, int size, boolean b){
+        block = new char[16];
+        if (size != 128 || size != 192 || size != 256) {
+            size = 128;
+        }
+        key = new char[size/8];
         message = msg;
+        for (int i = 0; i < 16; i++) {
+            block[i] = msg.charAt(i);
+        }
+        for (int j = 0; j < size/8; j++) {
+            key[j] = k.charAt(j);
+        }
         isEncoded = b;
+        //String result = toHex(message.getBytes());
+        //System.out.println(result);
     }
     
     public String getMessage(){
@@ -28,6 +43,18 @@ public class AES {
     
     public boolean getIsEncoded(){
         return isEncoded;
+    }
+    
+    private String toHex(byte [] bytes){
+        String result = "";
+        int decimal = 0;
+        String hex = "";
+        for (byte b : bytes) {
+            decimal = (int) b & 0xff;
+            hex = Integer.toHexString(decimal);
+            result += hex;
+        }
+        return result;
     }
     
     public void changeMessage(){
--- src/main/java/com/mycompany/mavenproject1/Main.java
+++ src/main/java/com/mycompany/mavenproject1/Main.java
@@ -11,9 +11,33 @@ package com.mycompany.mavenproject1;
  */
 public class Main {
     public static void main(String[] args){
+        int hex1 = 0xFF;
+        int hex2 = 0x00;
+        int hex3 = hex2+1;
+        String s = "abcdefghijklmnop";
+        String r = "";
+        byte [] result = s.getBytes();
+        for (byte b : result) {
+            int decimal = (int) b & 0xff;
+            String hex = Integer.toHexString(decimal);
+            r += hex;
+        }
+        //System.out.println(result);
+        //System.out.println(r);
         
+        char [] tab = new char[10];
+        for (int i = 0; i < 10; i++) {
+            tab[i] = s.charAt(i);
+        }
+        //tab = s.toCharArray();
+        System.out.println(tab);
+        
+        //AES aes = new AES(s, true);
+        
+        
+        /*
         System.out.println("Hello Maven!");
         new MainFrame().setVisible(true);
-        
+        */
     }
 }
