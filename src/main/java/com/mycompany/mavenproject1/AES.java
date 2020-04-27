74403410e744eaad3a57014216dee99ae4d48ea1
Jakub Strawa <j.strawa@stud.elka.pw.edu.pl>
2020-04-27 18:35:20 +0200

    AES class base methods added

--- src/main/java/com/mycompany/mavenproject1/AES.java
+++ src/main/java/com/mycompany/mavenproject1/AES.java
@@ -12,25 +12,49 @@ package com.mycompany.mavenproject1;
 public class AES {
     private String message;
     private boolean isEncoded;
+    private int keySize;
     private char [] block, key;
         
     
     public AES(String msg, String k, int size, boolean b){
         block = new char[16];
-        if (size != 128 || size != 192 || size != 256) {
-            size = 128;
+        keySize = size;
+        if (size != 128 && size != 192 && size != 256) {
+            keySize = 128;
         }
-        key = new char[size/8];
+        key = new char[keySize/8];
         message = msg;
-        for (int i = 0; i < 16; i++) {
-            block[i] = msg.charAt(i);
-        }
-        for (int j = 0; j < size/8; j++) {
-            key[j] = k.charAt(j);
-        }
+        fillTable(block, 16, msg);
+        fillTable(key, keySize/8, k);
         isEncoded = b;
         //String result = toHex(message.getBytes());
         //System.out.println(result);
+    }
+    
+    private void keyExpansion(){
+        
+    }
+    private void addRoundKey(){
+        
+    }
+    private void subBytes(){
+        
+    }
+    private void shiftRows(){
+        
+    }
+    private void mixColumns(){
+        
+    }
+    
+    private void fillTable(char [] tab, int size, String key){
+        for (int i = 0; i < size; i++) {
+            try {
+                tab[i] = key.charAt(i);
+            } catch (Exception e) {
+                tab[i] = ' ';
+            }
+        }
     }
     
     public String getMessage(){
@@ -58,7 +82,23 @@ public class AES {
     }
     
     public void changeMessage(){
-        
+        int rounds = 10;
+        if (keySize == 256) {
+            rounds = 14;
+        } else if (keySize == 192) {
+            rounds = 12;
+        } 
+        keyExpansion();
+        addRoundKey();
+        for (int i = 0; i < rounds - 1; i++) {
+            subBytes();
+            shiftRows();
+            mixColumns();
+            addRoundKey();
+        }
+        subBytes();
+        shiftRows();
+        addRoundKey();
     }
     
 }
