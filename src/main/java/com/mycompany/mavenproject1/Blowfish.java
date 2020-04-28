e1eb77e2a4e2b217fc63f509a61fcd704337ef1d
Jakub Strawa <j.strawa@stud.elka.pw.edu.pl>
2020-04-28 20:38:46 +0200

    Blowfish almost working

--- src/main/java/com/mycompany/mavenproject1/Blowfish.java
+++ src/main/java/com/mycompany/mavenproject1/Blowfish.java
@@ -104,19 +104,63 @@ public class Blowfish {
         sum += tab[3];
         return sum;
     }
+    private int sumBitsTable(byte [] tab){
+        int sum = 0;
+        sum = tab[0] << 7;
+        sum += tab[1] << 6;
+        sum += tab[2] << 5;
+        sum += tab[3] << 4;
+        sum += tab[4] << 3;
+        sum += tab[5] << 2;
+        sum += tab[6] << 1;
+        sum += tab[7];
+        return sum;
+    }
     
-    private void fFunction(String s){
-        System.out.println(s);
+    private int fFunction(String s){
+        //char [] tab = s.toCharArray();
+        String str = s;
+        byte [] box1,box2,box3,box4;
+        int sum1,sum2,sum3,sum4;
+        long modulo = 1L;
+        for (int i = 0; i < 32; i++) {
+            modulo = modulo << 1;
+        }
+        int s1,s2,s3;
+        if (str.length() == 32) {
+            sum1 = sumBitsTable(str.substring(0, 8).getBytes());
+            sum2 = sumBitsTable(str.substring(8, 16).getBytes());
+            sum3 = sumBitsTable(str.substring(16, 24).getBytes());
+            sum4 = sumBitsTable(str.substring(24, 32).getBytes());
+        } else if (s.length() < 32) {
+            while (str.length() != 32) {                
+                str = "0" + str;
+            }
+            sum1 = sumBitsTable(str.substring(0, 8).getBytes());
+            sum2 = sumBitsTable(str.substring(8, 16).getBytes());
+            sum3 = sumBitsTable(str.substring(16, 24).getBytes());
+            sum4 = sumBitsTable(str.substring(24, 32).getBytes());
+        } else {
+            System.err.println("Array longer than 32");
+            return 0;
+        }
+        s1 = (int) ((sum2 + sum1) % modulo);
+        s2 = sum3 ^ s1;
+        s3 = (int) ((s2 + sum4) % modulo);
+        System.out.println(s3);
+
+        return s3;
     }
     
     public void changeMessage(){
         byte [] lBlock, rBlock;
+        String left,right;
         lBlock = new byte[4];
         rBlock = new byte[4];
-        int leftSum = 0;
-        int rightSum = 0;
-        //for (int i = 0; i < 16; i++) {
-            lBlock[0] = block[0];
+        long leftSum = 0L;
+        long rightSum = 0L;
+        long tempSum = 0L;
+        lBlock[0] = block[0];
             lBlock[1] = block[1];
             lBlock[2] = block[2];
             lBlock[3] = block[3];
@@ -126,11 +170,31 @@ public class Blowfish {
             rBlock[3] = block[7];
             leftSum = sumTable(lBlock);
             rightSum = sumTable(rBlock);
-            leftSum = leftSum ^ mixedSubkeys.get(0);
-            fFunction(Integer.toBinaryString(leftSum));
+        for (int i = 0; i < 16; i++) {
             
-            
-        //}
+            leftSum = leftSum ^ mixedSubkeys.get(i);
+            tempSum = leftSum;
+            leftSum = fFunction(Integer.toBinaryString((int)leftSum));
+            leftSum = leftSum ^ rightSum;
+            rightSum = tempSum;
+//            left = Integer.toBinaryString(leftSum);
+//            right = Integer.toBinaryString(rightSum);
+            System.out.println(leftSum +"  "+rightSum);
+//            while (left.length() != 32) {                
+//                left = "0" + left;
+//            }
+//            while (right.length() != 32) {                
+//                right = "0" + right;
+//            }
+        }
+        leftSum ^= mixedSubkeys.get(17);
+        rightSum ^= mixedSubkeys.get(16);
+        System.out.println(leftSum +"  "+rightSum);
+        long finalResult = 0L;
+        finalResult += rightSum;
+        finalResult = finalResult << 32;
+        finalResult += leftSum;
+        System.out.println("Fianl code: " + finalResult);
     }
     
 }
